d0600ed808bd3caba3d327eec91a028d9a54e673
Priyam Shaw <priyam.shaw@myntra.com>
2016-08-31 17:12:30 +0530

    done

--- components/ImageEditor.js
+++ components/ImageEditor.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Canvas from './utils/canvas';
-import '../stylesheets/imageeditor.scss';
+import './stylesheets/imageeditor.scss';
 
 const deviceEvents = {
     react: {
