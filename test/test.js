ab2b08076e533065883c85871959ba2eacbb9202
Priyam Shaw <priyam.shaw@myntra.com>
2016-09-01 11:21:46 +0530

    test change

--- test/test.js
+++ test/test.js
@@ -2,4 +2,22 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import ImageEditor from '../components/ImageEditor';
 
-ReactDOM.render(<ImageEditor src="http://assets.myntassets.com/w_980,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2016/8/18/11471539799577-Slideshow-1920-x-1080-px--3-.jpg" customAspectRatio={true} />, document.getElementById('app-container'));
+var TestModule = React.createClass({
+	
+	onSaveEditedImage(img) {
+	    window.open(img)
+	},
+
+    render() {
+		return(
+			<ImageEditor src="http://assets.myntassets.com/w_980,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2016/8/18/11471539799577-Slideshow-1920-x-1080-px--3-.jpg" 
+				customAspectRatio={true} 
+				onSave={ this.onSaveEditedImage.bind(this) }
+			/>
+		);
+	}
+});
+
+ReactDOM.render(
+	<TestModule />, document.getElementById('app-container')
+);
